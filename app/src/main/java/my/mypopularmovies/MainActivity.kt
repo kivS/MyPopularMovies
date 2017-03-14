ce34b17cf26db28e0769d16b06ebc3eccd684392
Vik Borges <v1k@protonmail.com>
2017-03-14 16:41:39 +0000

    added background task; API implementation

--- app/src/main/java/my/mypopularmovies/MainActivity.kt
+++ app/src/main/java/my/mypopularmovies/MainActivity.kt
@@ -1,21 +1,54 @@
 package my.mypopularmovies
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import my.mypopularmovies.helpers.MoviesAPI
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = this.javaClass.simpleName
-    private var API_KEY: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        API_KEY = getString(R.string.the_movie_db_api)
+        // Load data
+        val moviesToFetch: String = MoviesAPI.moviesNowPlayingUrl()
 
-        Log.d(TAG, API_KEY)
+        FetchMoviesTask().execute(moviesToFetch)
 
     }
+
+    inner class FetchMoviesTask() : AsyncTask<String, Void, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String): String? {
+            if(params.size == 0) return null
+
+            // Get url to be fetched
+            val movieUrl: String = params[0]
+
+            try {
+
+                val queryResponse: String = MoviesAPI.getMovies(this@MainActivity, movieUrl)
+
+                return queryResponse
+
+
+            }catch (e: Exception){
+                e.printStackTrace()
+                return null
+            }
+        }
+
+        override fun onPostExecute(result: String?) {
+            Log.d(TAG, "Result: \n $result")
+        }
+    }
 }
+
+
--- app/src/main/java/my/mypopularmovies/helpers/MoviesAPI.kt
+++ app/src/main/java/my/mypopularmovies/helpers/MoviesAPI.kt
@@ -1,29 +1,77 @@
 package my.mypopularmovies.helpers
 
+import android.content.Context
+import android.util.Log
+import my.mypopularmovies.R
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+
 
 /**
  * Created by flying_potato on 10/03/2017.
  */
 class MoviesAPI {
-    companion object get{
-        private val BASE_URL = "https://api.themoviedb.org/3"
-        private val IMAGE_BASE_URL =  "https://image.tmdb.org/t/p"
+    companion object{
+        private val TAG = this.javaClass.simpleName
+        val BASE_URL = "https://api.themoviedb.org/3"
+        val IMAGE_BASE_URL =  "https://image.tmdb.org/t/p"
 
-        fun moviesNowPlayingUrl(API_KEY: String): String{
-           return "$BASE_URL/movie/now_playing?api_key=$API_KEY"
+
+        fun moviesNowPlayingUrl(): String{
+           return "$BASE_URL/movie/now_playing"
         }
 
-        fun moviesPopularUrl(API_KEY: String): String{
-            return "$BASE_URL/movie/popular?api_key=$API_KEY"
+        fun moviesPopularUrl(): String{
+            return "$BASE_URL/movie/popular"
         }
 
 
-        fun moviesTopRatedUrl(API_KEY: String): String{
-            return "$BASE_URL/movie/top_rated?api_key=$API_KEY"
+        fun moviesTopRatedUrl(): String{
+            return "$BASE_URL/movie/top_rated"
         }
 
         fun imageUrl(width: String = "w185", img: String ): String{
             return "$IMAGE_BASE_URL/$width/$img"
+
+        }
+
+
+        fun parseStringToJson(stringObj: String): Any{
+
+            var parsedObj: JSONObject = JSONObject(stringObj)
+
+            return parsedObj
+        }
+
+
+        /**
+         *  GET request for movies
+         */
+        fun getMovies(context: Context, MoviesUrl: String): String{
+
+            // Get API
+            val  API_KEY = context.getString(R.string.the_movie_db_api)
+            Log.d(TAG, "API_KEY: $API_KEY")
+
+            // Add API KEY to url
+            val url = "$MoviesUrl?api_key=$API_KEY"
+
+            val client: OkHttpClient = OkHttpClient()
+
+            // build get request url
+            val request: Request = Request.Builder()
+                    .url(url)
+                    .build()
+
+            // Execute request
+            val response: Response = client.newCall(request).execute()
+
+
+            // Get response body
+            return response.body().string()
+
 
         }
     }
