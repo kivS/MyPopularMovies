a87dd0a825a63040c4c05c98b57940889d06c6aa
Vik Borges <v1k@protonmail.com>
2017-03-17 18:04:39 +0000

    detail activity - data saved to state

--- app/src/main/java/my/mypopularmovies/DetailActivity.kt
+++ app/src/main/java/my/mypopularmovies/DetailActivity.kt
@@ -12,45 +12,65 @@ class DetailActivity : AppCompatActivity() {
 
     val TAG = javaClass.simpleName
 
+    var movie: JSONObject = JSONObject()
+    var posterUrl: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        // Check if intent has movie in payload
-        if(intent.hasExtra("movie")){
+        // Check if intent has movie in payload and if theres no data saved to the activity
+        if(intent.hasExtra("movie") && savedInstanceState == null){
 
             // Get movie object from payload
-            val movie: JSONObject = JSONObject(intent.getStringExtra("movie"))
-
+            movie = JSONObject(intent.getStringExtra("movie"))
             Log.d(TAG, "Movie detail of: ${movie.get("original_title")}")
 
-            try {
-
-                // Get poster Image
-                val posterUrl: String = MoviesAPI.getImageUrl(width = "w342", img = movie.getString("poster_path"))
-                Log.d(TAG, "movie poster url: $posterUrl")
-
-                // Set poster image
-                Picasso.with(this).load(posterUrl).into(iv_movie_poster)
-
-                // set movie title
-                tv_movie_title.text = movie.getString("original_title")
-
-                // set vote average
-                tv_movie_rating.text = "${movie.getString("vote_average")}/10"
-
-                // set release date
-                tv_movie_date.text = movie.getString("release_date")
-
-                // set description
-                tv_movie_synopsis.text = movie.getString("overview")
+            // Get poster Image
+            posterUrl = MoviesAPI.getImageUrl(width = "w342", img = movie.getString("poster_path"))
+            Log.d(TAG, "movie poster url: $posterUrl")
 
 
-            }catch (e: Exception){
-                e.printStackTrace()
+        }else{
+            if (savedInstanceState != null){
+                // get data from saved state
+                movie = JSONObject(savedInstanceState.getString("MOVIE_DATA"))
+                posterUrl = savedInstanceState.getString("MOVIE_POSTER_URL")
             }
-
-
         }
+
+
+        // Set data to views
+        try {
+
+            // Set poster image
+            Picasso.with(this).load(posterUrl).into(iv_movie_poster)
+
+            // set movie title
+            tv_movie_title.text = movie.getString("original_title")
+
+            // set vote average
+            tv_movie_rating.text = "${movie.getString("vote_average")}/10"
+
+            // set release date
+            tv_movie_date.text = movie.getString("release_date")
+
+            // set description
+            tv_movie_synopsis.text = movie.getString("overview")
+
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        // Save movie object to state
+        outState?.putString("MOVIE_DATA", movie.toString())
+
+        // save poster image url
+        outState?.putString("MOVIE_POSTER_URL", posterUrl)
     }
 }
